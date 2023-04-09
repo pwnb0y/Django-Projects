8cfc5808ca8af8aa944b80711af2eb2efc94cdde
pwnb0y <vickykr07@yahoo.com>
2023-04-09 23:47:25 +0530

    Create function is implemented

--- CRUD/enroll/admin.py
+++ CRUD/enroll/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+from  .models import User
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','name','email','password')
--- CRUD/enroll/views.py
+++ CRUD/enroll/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-
+from .forms import StudentRegistration
+from .models import User
 # Create your views here.
 def add_show(request):
-    return render(request, 'enroll/addandshow.html')
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            #fm.save
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email'] 
+            pw = fm.cleaned_data['password']
+            reg = User(name=nm,email=em,password=pw)
+            reg.save()
+    else:
+        fm = StudentRegistration()
+
+    return render(request, 'enroll/addandshow.html',{'form':fm})
